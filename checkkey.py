02305bb4106bfea5c18c805d756f9bcb98cb2204
mitesh kumar <miteshsutha7297@gmail.com>
2022-02-08 22:46:18 +0530

    changes in admin control

---
+++ checkkey.py
@@ -0,0 +1,15 @@
+def checkkey(arr,n,key,segment):
+    for i in range(0,n,segment):
+        for j in range(0,segment):
+            if i+j<n and arr[i+j]==key:
+                j=segment
+                break
+        if j==segment-1:
+            return False
+    return True
+arr=[3,5,2,4,9,3,1,7,3,11,12,3]
+key=3
+segment=4
+n=len(arr)
+result=checkkey(arr,n,key,segment)
+print(result)
